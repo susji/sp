78e7ddfe6fdf02da2742180a3606f8948109e32e
susji <susji@protonmail.com>
2021-11-16 14:24:16 +0200

    deploy: certbot needs to run last

--- deploy/x.sh
+++ deploy/x.sh
@@ -16,18 +16,19 @@ install -v -m 0644 -o root -g root sp.service /etc/systemd/system/sp.service
 
 adduser --force-badname --system --no-create-home --group _sp
 
-certbot --nginx -d $YOUR_DOMAIN
-certbot renew --dry-run
+install -m 0644 -o root -g root yourdomain /etc/nginx/sites-available/$YOUR_DOMAIN
 
 systemctl daemon-reload
 systemctl enable sp.service
 systemctl start sp.service
 systemctl status sp.service
 
-install -m 0644 -o root -g root yourdomain /etc/nginx/sites-available/$YOUR_DOMAIN
 
 sed -i -E \
     "s|yourdomain|${YOUR_DOMAIN}|g" \
     /etc/nginx/sites-available/$YOUR_DOMAIN
 ln -s /etc/nginx/sites-available/$YOUR_DOMAIN /etc/nginx/sites-enabled
 rm -f /etc/nginx/sites-enabled/default
+
+certbot --nginx -d $YOUR_DOMAIN
+certbot renew --dry-run
